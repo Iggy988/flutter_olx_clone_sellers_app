81efbdde2a8dccbce78ec4a640c3396bd43dd7f5
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-08-28 09:04:44 +0200

    Update global.dart

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -5,4 +5,4 @@ SharedPreferences? sharedPreferences;
 CartMethods cartMethods = CartMethods();
 String previousEarning = '';
 
-String fcmServerToken = 'key=AAAAHD3IhJ8:APA91bHLwDojJC-Wpne5o9HleoFYUya1pNsy7_EITaoGCOVpHaOaNcCVfYfJMlR-O6hhVDSVE7XJEcoFUECZgUHVZO3nIbKoYaRPhdcOmzGhY9b95qZMdTBc28HNkBj4CKPZvHrH4hCQ';
+String fcmServerToken = '';
